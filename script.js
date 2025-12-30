f392288872e7ad99c18740d15ff63fc368605d30
Xevinty-github <127631904+Xevinty-github@users.noreply.github.com>
2025-12-30 00:55:28 -0800

    Change version number format in script.js
    Updated version number format from '0.1.2a' to '1.00.004'.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let lines_counter = 0;
 const lines_counter_reference = document.getElementById("lines-counter");
-const version_number = "0.1.2a";
+const version_number = "1.00.004"; // change x.y.z(s) to x.yy(.zzz)
 
 function IncreaseMain() {
     lines_counter += 1;
